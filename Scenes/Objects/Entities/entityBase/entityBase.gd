8ae1db86d5b380428f7e083390a90ed137b15088
RyuukoAmi <117397488+RyuukoAmi@users.noreply.github.com>
2024-03-16 15:49:47 -0600

    16-03-2024-15-49

--- Scenes/Objects/Entities/entityBase/entityBase.gd
+++ Scenes/Objects/Entities/entityBase/entityBase.gd
@@ -95,15 +95,15 @@ func char_movement(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_backward", "move_forward", "move_left", "move_right")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if Input.is_action_just_pressed("dash"):
+	var _direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#if Input.is_action_just_pressed("dash"):
 		
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
 
 func char_facing():
 	#_______________ INFO _________________
